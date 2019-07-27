12a2b4b7c5e2e3b58535a34efc6740b6826b75c8
LinkClink <shekaqwe@gmail.com>
2019-07-27 15:39:42 +0200

    Change and develop new activity

--- app/src/main/java/com/example/linkclink/pocketguidemini_wariors_game__beta_ver_01/SkillsItemsMemuActivity_3.java
+++ app/src/main/java/com/example/linkclink/pocketguidemini_wariors_game__beta_ver_01/SkillsItemsMemuActivity_3.java
@@ -1,5 +1,6 @@
 package com.example.linkclink.pocketguidemini_wariors_game__beta_ver_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,20 @@ import android.widget.Button;
 public class SkillsItemsMemuActivity_3 extends AppCompatActivity
 {
 
-    private Button menu_button_1;
+    int item_type = 1;
+    String ac_request_code = "512";
+
+    Intent intent;
+
+    int i=0;
+
+    Button[] buttons = new Button[1];
+
+    String[] table_names;
+    String[] type_names;
+    String type_1;
+
+    int resID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,19 +31,37 @@ public class SkillsItemsMemuActivity_3 extends AppCompatActivity
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_skills_items_memu_3);
+
+        buttons_realisations();
     }
 
-    public void onClick(View view)
-    {
-        menu_button_1 = findViewById(R.id.menuscrolls_button_0);
+   public void buttons_realisations()
+   {
+       table_names = getResources().getStringArray(R.array.Scroll_Table_name);
+       type_names = getResources().getStringArray(R.array.Scroll_type_name);
+       type_1 = getResources().getString(R.string.Scrolls_type_1);
 
-        if(view == menu_button_1) ;
+       for(i=0;i<1;i++)
+       {
+           resID = getResources().getIdentifier("menuscrolls_button_" + i, "id", getPackageName());
+           buttons[i] = (Button) findViewById(resID);
+           buttons[i].setId(i);
+           buttons[i].setOnClickListener(new View.OnClickListener()
+           {
+               @Override
+               public void onClick(View v)
+               {
+                   intent = new Intent(SkillsItemsMemuActivity_3.this, ItemsListActivity_4.class);
+                   intent.putExtra("table_name",table_names[v.getId()]);
+                   intent.putExtra("type_1",type_1);
+                   intent.putExtra("item_type",item_type);
+                   intent.putExtra("request_code",ac_request_code);
+                   startActivityForResult(intent,1);
+                   overridePendingTransition(R.anim.animation_activity_standart_1,R.anim.animation_activity_standart_1);
+               }
+           });
+       }
 
-
-
-
-    }
-
-
+   }
 
 }
